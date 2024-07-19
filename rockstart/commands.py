60b64fb7db755c4381930048aca15c21ba574fec
aldyahsn <aldy.ahsandin@gmail.com>
2024-07-19 23:45:11 +0700

    add deploy command

--- rockstart/commands.py
+++ rockstart/commands.py
@@ -1,6 +1,8 @@
 # rockstart/commands.py
 import shutil
 import os
+import subprocess
+
 
 def copy_project_files():
     current_dir = os.path.dirname(__file__)
@@ -12,3 +14,78 @@ def copy_project_files():
         print('Project files copied successfully.')
     except Exception as e:
         print(f'Error: {e}')
+        
+def deploy(domain_name):
+    current_dir = os.getcwd()
+
+    # Create the socket file
+    socket_content = f"""
+        [Unit]
+        Description=Socket for {domain_name}
+
+        [Socket]
+        ListenStream=/run/{domain_name}.sock
+
+        [Install]
+        WantedBy=sockets.target
+        """
+    socket_path = f"/home/dev/{domain_name}.socket"
+    # socket_path = f"/etc/systemd/system/{domain_name}.socket"
+    with open(socket_path, 'w') as socket_file:
+        socket_file.write(socket_content)
+    print(f"Created socket file at {socket_path}")
+
+    # Create the service file
+    service_content = f"""
+    
+        [Unit]
+        Description=gunicorn daemon
+        Requires={domain_name}.socket
+        After=network.target
+
+        [Service]
+        User=dev
+        Group=www-data
+        WorkingDirectory=/srv/{domain_name}
+        ExecStart=/srv/{current_dir}/.venv/bin/gunicorn \
+        --access-logfile - \
+        --workers 3 \
+        --bind unix:/run/{domain_name}.sock \
+        --chdir /srv/{domain_name} \
+        config.wsgi:application
+        """
+        
+    service_path = f"/home/dev/{domain_name}.service"
+    # service_path = f"/etc/systemd/system/{domain_name}.service"
+    with open(service_path, 'w') as service_file:
+        service_file.write(service_content)
+    print(f"Created service file at {service_path}")
+
+    # Start and enable the service
+    # subprocess.run(['sudo', 'systemctl', 'start', domain_name])
+    # subprocess.run(['sudo', 'systemctl', 'enable', domain_name])
+    print(f"Started and enabled service for {domain_name}")
+
+    # # Create the nginx config file
+    # nginx_content = f"""server {{
+    #     listen 80;
+    #     server_name {domain_name};
+
+    #     location / {{
+    #         proxy_pass http://unix:/run/{domain_name}.sock;
+    #         proxy_set_header Host $host;
+    #         proxy_set_header X-Real-IP $remote_addr;
+    #         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #         proxy_set_header X-Forwarded-Proto $scheme;
+    #     }}
+    # }}
+    # """
+    # nginx_path = f"/etc/nginx/sites-available/{domain_name}"
+    # with open(nginx_path, 'w') as nginx_file:
+    #     nginx_file.write(nginx_content)
+    # os.symlink(nginx_path, f"/etc/nginx/sites-enabled/{domain_name}")
+    # print(f"Created nginx config file at {nginx_path}")
+
+    # # Restart nginx to apply the changes
+    # subprocess.run(['sudo', 'systemctl', 'restart', 'nginx'])
+    # print("Restarted nginx")
