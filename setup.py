067a419bc63ff62dffc90ba394659d541606dca4
aldyahsn <aldy.ahsandin@gmail.com>
2024-07-19 22:40:18 +0700

    increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rocket-starter',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'django>=3.0',
