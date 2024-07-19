95cf4cb6170398f27acb4e55557dfaadc17d37e7
aldyahsn <aldy.ahsandin@gmail.com>
2024-07-19 23:47:34 +0700

    add cli

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rockstart',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
