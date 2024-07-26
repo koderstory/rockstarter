2bf27d26679aa0526b44b53c77148badac59c81b
Aldy <aldy.ahsandin@gmail.com>
2024-07-26 13:15:52 +0700

    Update commands.py

--- rockstart/commands.py
+++ rockstart/commands.py
@@ -16,21 +16,12 @@ def create_virtualenv(directory):
 
 def copy_project_files():
     current_dir = os.path.dirname(__file__)
-    template_dir = os.path.join(current_dir, 'projectfiles')
+    template_dir = os.path.join(current_dir, 'myproject')
     target_dir = os.getcwd()
 
     try:
-        if os.path.exists(template_dir):
-            for item in os.listdir(template_dir):
-                s = os.path.join(template_dir, item)
-                d = os.path.join(target_dir, item)
-                if os.path.isdir(s):
-                    shutil.copytree(s, d, dirs_exist_ok=True)
-                else:
-                    shutil.copy2(s, d)
-            print('Project files copied successfully.')
-        else:
-            print(f"Template directory {template_dir} does not exist.")
+        shutil.copytree(template_dir, target_dir, dirs_exist_ok=True)
+        print('Project files copied successfully.')
     except Exception as e:
         print(f'Error: {e}')
 
