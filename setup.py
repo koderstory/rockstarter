fa84e48b721d128a7b24a81fb7615dd411064abf
Aldy <aldy.ahsandin@gmail.com>
2024-08-20 14:00:30 +0700

    Update setup version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rockstarter',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'Django>=4.2.15',
