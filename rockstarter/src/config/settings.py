1d857ec3fbceaca4ab5ab713101edd0dd97b660e
aldyahsn <aldy.ahsandin@gmail.com>
2024-09-26 19:14:53 +0700

    update .env sample

--- rockstarter/src/config/settings.py
+++ rockstarter/src/config/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 if env.bool('IS_S3'):
     AWS_ACCESS_KEY_ID = env('ACCESS_KEY')
     AWS_SECRET_ACCESS_KEY = env('SECRET_KEY')
-    AWS_STORAGE_BUCKET_NAME = "hamesha-catalog"
+    AWS_STORAGE_BUCKET_NAME = env('BUCKET_NAME')
     AWSS_S3_SIGNATURE_VERSION = 's3v4'
     AWS_S3_REGION_NAME = 'singapore'
     AWS_S3_FILE_OVERWRITE = False
@@ -157,7 +157,7 @@ STORAGES = {
     },
 }
 
-if env("IS_S3"):
+if env.bool("IS_S3"):
     STORAGES["default"]["BACKEND"] = "storages.backends.s3boto3.S3Boto3Storage"
 
 # Default primary key field type
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rockstarter',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     install_requires=[
         'Django>=4.2.15',
